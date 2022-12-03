1306f68535b469af6690fc4c2546cf5bb1bbbb44
benyuter <99632550+benyuter@users.noreply.github.com>
2022-12-03 17:56:46 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!-- Jayden Cooper 11/02/2022, Ileaqua Adams 11/02/2022, Ben Yuter 12/03/2022, John Giaquinto 11/07/2022 -->
+<?php include 'header.php'; ?>
+<main>
+    <nav>
+
+        <h2>Administrators</h2>
+        <ul>
+            <li><a href="product_manager/index.php">Manage Products</a></li>
+            <li><a href="technician_manager/index.php">Manage Technicians</a></li>
+            <li><a href="customer_manager/index.php">Manage Customers</a></li>
+            <li><a href="incident_manager/index.php">Create Incident</a></li>
+            <li><a href="under_construction.php">Assign Incident</a></li>
+            <li><a href="under_construction.php">Display Incidents</a></li>
+        </ul>
+
+        <h2>Technicians</h2>
+        <ul>
+            <li><a href="under_construction.php">Update Incident</a></li>
+        </ul>
+
+        <h2>Customers</h2>
+        <ul>
+            <li><a href="product_register/index.php">Register Product</a></li>
+        </ul>
+
+    </nav>
+</main>
+<?php include 'view/footer.php'; ?>
+
