8fea3c5f0906aa19f8fcf010e9d06000afcdba87
jgia <55515426+jgia@users.noreply.github.com>
2022-12-17 23:34:32 -0500

    Delete logout.php

--- tech_support/product_register/logout.php
+++
@@ -1,7 +0,0 @@
-<!-- Jayden Cooper 11/30/2022 -->
-<?php
-session_start();       //resume current session
-session_unset();      //remove session variables
-session_destroy();    //destroy session
-header("Location: index.php");
-?>
